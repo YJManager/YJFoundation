af7b24a5ac9e6db43bd83b8ab48eb4fe099bdce5
yuejun.hou <8069060@qq.com>
2017-05-27 11:23:27 +0800

    initial

---
+++ YJFoundation/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  YJFoundation
+//
+//  Created by YJHou on 2017/5/27.
+//  Copyright © 2017年 YJHou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ YJFoundation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YJFoundation
+//
+//  Created by YJHou on 2017/5/27.
+//  Copyright © 2017年 YJHou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
